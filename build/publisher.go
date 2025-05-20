51711345fad80ca16d9897a94f79315ab0ea395e
Johan Wassberg <jocar@sunet.se>
2025-05-20 13:02:06 +0200

    200 is the default

--- build/publisher.go
+++ build/publisher.go
@@ -46,7 +46,6 @@ func (m *myMux) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		// Handle Haproxy's default method and path for Health checks
 		// https://www.haproxy.com/documentation/haproxy-configuration-tutorials/reliability/health-checks/#http-health-checks
 		if req.Method == "OPTIONS" && reqFile == "/" {
-			w.WriteHeader(http.StatusOK)
 			w.Write([]byte("Meep meep"))
 
 			return
